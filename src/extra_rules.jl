f3480c83e0aeed2151bc5ed72e048037c0dcb0a7
Mykola Lukashchuk nikola.lukashuk@gmail.com <nikola.lukashuk@gmail.com>
2023-04-22 09:56:23 +0200

    fix: precisely difiend set of rules

--- src/extra_rules.jl
+++ src/extra_rules.jl
@@ -10,34 +10,10 @@ end
     return GammaShapeRate(1000, 1)
 end
 
-@rule GammaShapeRate(:β, Marginalisation) (q_out::GammaShapeRate, q_α::GammaShapeRate, ) = begin 
-    return GammaShapeRate(1000, 1)
-end
-
-@rule GammaShapeRate(:α, Marginalisation) (q_out::Gamma, q_β::GammaShapeRate, ) = begin 
-    return GammaShapeRate(1000, 1)
-end
-
 @rule GammaShapeRate(:α, Marginalisation) (q_out::GammaShapeRate, q_β::GammaShapeRate, ) = begin 
     return GammaShapeRate(1000, 1)
 end
 
-@rule GammaShapeRate(:out, Marginalisation) (q_α::PointMass, q_β::GammaShapeRate, ) = begin 
+@rule GammaShapeRate(:out, Marginalisation) (q_α::Any, q_β::Any, ) = begin 
     return GammaShapeRate(mean(q_α), mean(q_β))
 end
-
-@rule GammaShapeRate(:out, Marginalisation) (q_α::GammaShapeRate, q_β::GammaShapeRate, ) = begin 
-    return GammaShapeRate(mean(q_α), mean(q_β))
-end
-
-# @rule GammaShapeRate(:β, Marginalisation) (q_out::Any, q_α::Any, ) = begin 
-#     return GammaShapeRate(1000, 1)
-# end
-
-# @rule GammaShapeRate(:out, Marginalisation) (q_α::Any, q_β::Any, ) = begin 
-#     return GammaShapeRate(mean(q_α), mean(q_β))
-# end
-
-# @rule GammaShapeRate(:α, Marginalisation) (q_out::Any, q_β::Any, ) = begin 
-#     return GammaShapeRate(1000, 1)
-# end
